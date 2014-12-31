8ad9fad88e7ce9485efa8c718301661a392cbae3
Mattia Basaglia <mattia.basaglia@gmail.com>
2014-12-31 21:12:40 +0100

    Load SVG to show thee such greatness of our sparkly mane.

--- luna.js
+++ luna.js
@@ -24,11 +24,12 @@
 "use strict";
 
 var luna_settings = {
-	framerate  :  60, // frames per second
-	moon_speed :  12, // pixels / frame * image_resize_factor
-	moon_size  : 1.5, // multiplier
-	media      : "media/", // relative path
-	z_index    :   1, // base z-index 
+	framerate  :      60, // frames per second
+	moon_speed :      12, // pixels / frame * image_resize_factor
+	moon_size  :     1.5, // multiplier
+	media      :"media/", // path/prefix
+	z_index    :       1, // base z-index 
+	image_type :   'svg', // svg or png
 	shadow: {
 		move   : 1/3, // multiplier
 		opacity: 0.5, // alpha
@@ -53,7 +54,14 @@ var luna_settings = {
 			min_alpha    : 0,
 			max_alpha    : 0.8,
 		},
-	}
+	},
+	svg: {
+		star_scale_min: 0.5, // multiplier
+		star_scale_max: 2.5, // multiplier
+		star_scale_inc: 0.1, // multiplier increase
+		star_orbit    :   2, // increase in degree / frame
+		star_spin     :   5, // increase in degree / frame
+	},
 };
 
 function LunaSettings(change) {
@@ -65,23 +73,48 @@ function LunaSettings(change) {
 	return settings;
 }
 
+function SvgNsResolver(prefix) {
+	var xmlns = {
+		rdf  : "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
+		cc   : "http://creativecommons.org/ns#",
+		xlink: "http://www.w3.org/1999/xlink",
+		dc   : "http://purl.org/dc/elements/1.1/",
+		svg  : "http://www.w3.org/2000/svg"
+	};
+	return xmlns[prefix] || null;
+}
+
 function Luna(element, settings) {
 	
+	// Evaluates the alpha of a star component
 	this.StarAlpha = function (settings, distance) {
 		var alpha = 1 - Math.min(1,distance/settings.fade_distance);
 		return settings.min_alpha + (settings.max_alpha-settings.min_alpha) * alpha;
 	};
 
+	// Resizes Luna to fit in the window
 	this.EventResize = function () {
+		var luna_box = this.luna.getBoundingClientRect();
+		if (this.luna.image_type == 'svg') {
+			this.luna.x = luna_box.left;
+			this.luna.y = luna_box.top;
+		}
+		
 		this.area = element.getBoundingClientRect();
 		var wratio = this.area.width / this.luna.naturalWidth;
 		var hratio = (this.area.height-this.luna.y) / this.luna.naturalHeight;
 		this.imgratio = Math.min(wratio,hratio,1);
 		
-		var width = Math.round(this.luna.naturalWidth * this.imgratio);
+		var width = this.luna.naturalWidth * this.imgratio;
+		var x = (this.area.width - width) / 2;
 		this.luna.style.width = width + 'px';
 		this.luna.style.height = 'auto';
-		this.luna.style.left = (this.area.width - width) / 2 + "px";
+		this.luna.style.left = x + "px";
+		if (this.luna.image_type == 'svg') {
+			this.luna.width = width;
+			this.luna.height = Math.round(this.luna.naturalHeight * this.imgratio);
+			this.luna.x = x;
+		}
 		this.luna.client_x = this.luna.x + this.luna.width/2;
 		this.luna.client_y = this.luna.y + this.luna.height/2;
 		
@@ -97,6 +130,7 @@ function Luna(element, settings) {
 		};
 	}
 
+	// Interact with the mouse
 	this.EventMouseMove = function (event) {
 		var dx = (this.area.left + this.luna.client_x - event.clientX);
 		var dy = (this.area.top + this.luna.client_y - event.clientY);
@@ -110,7 +144,90 @@ function Luna(element, settings) {
 			this.stars[s].star_speed = this.settings.star.fade_speed;
 		}
 	}
+	
+	// Calculates scaling factor for a svg mane/tail star
+	this.SvgStarScale = function() {
+		return Math.random() * 
+			(this.settings.svg.star_scale_max - this.settings.svg.star_scale_min) +
+			this.settings.svg.star_scale_min;
+	}
+	
+	// Initializes interactions once the media have been loaded
+	// For each call of this.Ready() there should be a 
+	// this.loading++ in the appropriate location
+	this.Ready = function() {
+		this.loading--;
+		if (!this.loading) {
+			this.EventResize();
+			
+			this.moon.style.left = this.moon.restingpos.x+'px';
+			this.moon.style.top = this.area.height+'px';
+			this.moon.targetpos = { x: this.moon.restingpos.x, y: this.moon.restingpos.y };
+			
+			this.moon.style.visibility = 'visible';
+			this.shadow.style.visibility = 'visible';
+			this.luna.style.visibility = 'visible';
+			
+			if (this.luna.image_type == 'svg') {
+				var snapshot = document.evaluate(
+					'//svg:use[@xlink:href="#star_original"]', 
+					this.luna.svg, SvgNsResolver, 
+					XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null );
+				
+				this.mane_stars = [];
+				for (var i = 0; i < snapshot.snapshotLength; i++) {
+					var mane_star = snapshot.snapshotItem(i);
+					
+					var old_matrix = mane_star.transform.baseVal[0].matrix;
+					mane_star.transform.baseVal.clear();
+					
+					var translate = this.luna.svg.createSVGTransform();
+					translate.setTranslate(old_matrix.e,old_matrix.f);
+					mane_star.transform.baseVal.appendItem(translate);
+					
+					
+					var orbit = this.luna.svg.createSVGTransform();
+					orbit.setRotate(Math.random()*360,0,0);
+					mane_star.transform.baseVal.appendItem(orbit);
+					
+					var scale = this.luna.svg.createSVGTransform();
+					var scale_factor = this.SvgStarScale();
+					scale.setScale(scale_factor,scale_factor);
+					mane_star.transform.baseVal.appendItem(scale);
+					
+					
+					var spin = this.luna.svg.createSVGTransform();
+					spin.setRotate(Math.random()*360,0,0);
+					mane_star.transform.baseVal.appendItem(spin);
+					
+					mane_star.scale = {
+						transform: scale,
+						speed    : (Math.round(Math.random())*2-1) * this.settings.svg.star_scale_inc,
+						factor   : scale_factor
+					};
+					
+					mane_star.rotate = {
+						orbit      : orbit,
+						orbit_speed: (Math.random()*2-1)*this.settings.svg.star_orbit,
+						spin       : spin,
+						spin_speed : (Math.random()*2-1)*this.settings.svg.star_spin,
+					};
+					
+					this.mane_stars.push(mane_star);
+					
+					
+				}
+			}
+			
+			// events
+			setInterval(this.Step.bind(this), 1000/this.settings.framerate);
+			window.addEventListener("resize",this.EventResize.bind(this));
+			this.parent.addEventListener("mousemove",this.EventMouseMove.bind(this));
+			this.parent.addEventListener("mouseleave",this.EventMouseLeave.bind(this));
+		}
+	}
 
+	// Executes a frame
 	this.Step = function() {
 		var dx = this.moon.targetpos.x - this.moon.x;
 		var dy = this.moon.targetpos.y - this.moon.y;
@@ -177,8 +294,23 @@ function Luna(element, settings) {
 				this.stars[s].star_speed = this.settings.star.twinkle_speed;
 			}
 		}
+		
+		for (var s in this.mane_stars) {
+			var scale = this.mane_stars[s].scale;
+			scale.factor += scale.speed;
+			scale.transform.setScale(scale.factor,scale.factor);
+			if (scale.factor >= this.settings.svg.star_scale_max) {
+				scale.speed = -this.settings.svg.star_scale_inc;
+			} else if (scale.factor <= this.settings.svg.star_scale_min) {
+				scale.speed = this.settings.svg.star_scale_inc;
+			}
+			var rotate = this.mane_stars[s].rotate;
+			rotate.orbit.setRotate(rotate.orbit.angle+rotate.orbit_speed,0,0);
+			rotate.spin.setRotate(rotate.spin.angle+rotate.spin_speed,0,0);
+		}
 	}
 
+	// Creates a star
 	this.SpawnStar = function () {
 		var star_wrapper = document.createElement('div');
 		star_wrapper.className = "star";
@@ -231,17 +363,60 @@ function Luna(element, settings) {
 		this.stars.push(star_wrapper);
 	}
 
+	// Sets the moon to get back behind Luna
 	this.EventMouseLeave = function () {
 		this.moon.targetpos = { x: this.moon.restingpos.x, y: this.moon.restingpos.y };
 	}
-
-	this.SpawnLuna = function (parent) {
-		this.luna = document.createElement('img');
-		this.luna.src = this.settings.media+"luna.png";
-		this.luna.alt = "Best Princess"
-		parent.insertBefore(this.luna,null);
+	
+	// Sets style features common to SVG and PNG Luna image
+	this.SetLunaCommonStyle = function () {
+		this.luna.style.zIndex = this.settings.z_index+10;
+		this.luna.style.position = "absolute";
 	}
 	
+	// Executed when we receive Luna's SVG data
+	this.ReceiveSvgLuna = function (luna_request) {
+		if (luna_request.readyState == 4 && luna_request.status == 200) {
+			this.luna = document.createElement('div');
+			
+			this.luna.svg = luna_request.responseXML.documentElement;
+			this.luna.svg.style.width = '100%';
+			this.luna.svg.style.height = '100%';
+			
+			this.luna.naturalWidth = this.luna.svg.width.baseVal.value;
+			this.luna.naturalHeight = this.luna.svg.height.baseVal.value;
+			this.SetLunaCommonStyle();
+			this.luna.style.visibility = 'hidden';
+			this.luna.image_type = 'svg';
+			
+			this.luna.insertBefore(this.luna.svg,null);
+			this.parent.insertBefore(this.luna,null);
+			
+			this.Ready();
+		}
+	}
+
+	// Creates Luna's image
+	this.SpawnLuna = function (parent) {
+		if (this.settings.image_type == 'png') {
+			this.luna = document.createElement('img');
+			this.luna.src = this.settings.media+"luna.png";
+			this.luna.alt = "Best Princess"
+			this.luna.image_type = 'png';
+			this.loading++;
+			this.luna.onload = this.Ready.bind(this);
+			this.parent.insertBefore(this.luna,null);
+			this.SetLunaCommonStyle();
+		} else if (this.settings.image_type == 'svg') {
+			this.loading++;
+			var luna_request = new XMLHttpRequest();
+			luna_request.onreadystatechange = this.ReceiveSvgLuna.bind(this,luna_request);
+			luna_request.open("GET",this.settings.media+"luna.svg",true);
+			luna_request.send();
+		}
+	}
+	
+	// Creates the main menu
 	this.SpawnMainMenu = function() {
 		this.menu = document.createElement('menu');
 		this.menu.id = "luna-menu_"+Math.random().toString(16).slice(2);
@@ -250,15 +425,18 @@ function Luna(element, settings) {
 		//this.parent.contextMenu = this.menu;
 		this.parent.setAttribute("contextmenu",this.menu.id);
 	}
+	// Adds a menu item/submenu to a menu
 	this.InsertMenuItem = function(menu,item) {
 		menu.insertBefore(item,null);
 	}
+	// Creates a simple menu item
 	this.SpawnMenuItem = function(object) {
 		var menuitem = document.createElement('menuitem');
 		for (var key in object)
 			menuitem[key] = object[key];
 		return menuitem;
 	}
+	// Creates a sub menu
 	this.SpawnSubMenu = function(label) {
 		var menu = document.createElement('menu');
 		menu.label = label;
@@ -281,12 +459,9 @@ function Luna(element, settings) {
 	//misc
 	this.imgratio = 1;
 	this.area = element.getBoundingClientRect();
+	this.loading = 0;
 	// luna
-	this.luna = element.getElementsByClassName('luna')[0]; 
-	if (!this.luna)
-		this.SpawnLuna(element);
-	this.luna.style.zIndex = this.settings.z_index+10;
-	this.luna.style.position = "absolute";
+	this.SpawnLuna();
 	// shadow
 	this.shadow = document.createElement('div');
 	this.shadow.style.zIndex = this.settings.z_index+1;
@@ -294,6 +469,7 @@ function Luna(element, settings) {
 	this.shadow.style.backgroundColor = "black";
 	this.shadow.style.opacity = 0;
 	this.shadow.style.borderRadius = "100%";
+	this.shadow.style.visibility = 'hidden';
 	this.parent.insertBefore(this.shadow,null);
 	// moon
 	this.moon = document.createElement('img');
@@ -301,7 +477,10 @@ function Luna(element, settings) {
 	this.moon.style.position = "absolute";
 	this.moon.style.zIndex = this.settings.z_index+2;
 	this.moon.style.height = 'auto';
+	this.moon.style.visibility = 'hidden';
 	this.parent.insertBefore(this.moon,null);
+	this.loading++;
+	this.moon.onload = this.Ready.bind(this);
 	// stars
 	this.stars = [];
 	for (var i = 0; i < this.settings.star.number; i++)
@@ -338,17 +517,8 @@ function Luna(element, settings) {
 		type: "radio"
 	}));
 	this.InsertMenuItem(this.menu, bp_menu);
-	// events
-	this.moon.onload = function() {
-		this.EventResize();
-		this.moon.style.left = this.moon.restingpos.x+'px';
-		this.moon.style.top = this.area.height+'px';
-		this.moon.targetpos = { x: this.moon.restingpos.x, y: this.moon.restingpos.y };
-		setInterval(this.Step.bind(this), 1000/this.settings.framerate);
-	}.bind(this);
-	window.addEventListener("resize",this.EventResize.bind(this));
-	this.parent.addEventListener("mousemove",this.EventMouseMove.bind(this));
-	this.parent.addEventListener("mouseleave",this.EventMouseLeave.bind(this));
+	
+	
 } 
 
 function SetupLuna(element,settings) {
